eb747aa98eae4bf2549dd069e81e719b31379c40
linG5821 <3137168214@qq.com>
2021-02-07 19:40:27 +0800

    初始化创建

---
+++ senna-host/src/main/java/aiot/mclaren/senna/host/service/ICategoryService.java
@@ -0,0 +1,16 @@
+package aiot.mclaren.senna.host.service;
+
+import aiot.mclaren.senna.model.entity.Category;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lsj
+ * @since 2021-02-07
+ */
+public interface ICategoryService extends IService<Category> {
+
+}
